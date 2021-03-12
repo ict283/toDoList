6fabdf1793d5b0de7d89c248439968ad62c1e832
ict283 <glennlum@icloud.com>
2021-03-12 19:20:29 +0800

    Initial Commit

---
+++ toDoList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  toDoList
+//
+//  Created by Glenn Lum on 12/3/21.
+//  Copyright © 2021 Glenn Lum. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
